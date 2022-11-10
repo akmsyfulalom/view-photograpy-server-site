7fefa8c820f90014187ece7b7d99e011300f1e33
akmsyfulalom <akmsyfulbd12@gmail.com>
2022-11-10 23:39:30 +0600

    selete review

--- index.js
+++ index.js
@@ -72,6 +72,14 @@ async function run() {
             const result = await reviewsCollection.insertOne(review)
             res.send(result);
         })
+
+
+        app.delete('/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewsCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
